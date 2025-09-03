a6a67532caf84abd8b782eac30e1822d007bab39
TEJWARDEEP SINGH <tejwardeep@gmail.com>
2025-09-03 21:00:25 +0530

    some minor bugs fixed

--- public/javascripts/launcher.js
+++ public/javascripts/launcher.js
@@ -1,61 +1,61 @@
 function current_launchers(){
     var sslv=document.querySelector("#cb1")
     sslv.addEventListener("click",function(){
-        gsap.to(".r-panel h3",{
+        gsap.to("#current-launchers-wrapper .r-panel h3",{
             color:"#2c5364"
         })
-        gsap.to(".launcher-box",{
+        gsap.to("#current-launchers-wrapper .launcher-box",{
             display:"none"
         })
-        gsap.to("#r-sslv",{
+        gsap.to("#current-launchers-wrapper #r-sslv",{
             display:"flex",
         })
-        gsap.to("#cb1",{
+        gsap.to("#current-launchers-wrapper #cb1",{
             color:"#00bcd4"
         })
     })
     var pslv=document.querySelector("#cb2")
     pslv.addEventListener("click",function(){
-        gsap.to(".r-panel h3",{
+        gsap.to("#current-launchers-wrapper .r-panel h3",{
             color:"#2c5364"
         })
-        gsap.to(".launcher-box",{
+        gsap.to("#current-launchers-wrapper .launcher-box",{
             display:"none"
         })
-        gsap.to("#r-pslv",{
+        gsap.to("#current-launchers-wrapper #r-pslv",{
             display:"flex"
         })
-        gsap.to("#cb2",{
+        gsap.to("#current-launchers-wrapper #cb2",{
             color:"#00bcd4"
         })
     })
     var gslv=document.querySelector("#cb3")
     gslv.addEventListener("click",function(){
-        gsap.to(".r-panel h3",{
+        gsap.to("#current-launchers-wrapper .r-panel h3",{
             color:"#2c5364"
         })
-        gsap.to(".launcher-box",{
+        gsap.to("#current-launchers-wrapper .launcher-box",{
             display:"none"
         })
-        gsap.to("#r-gslv",{
+        gsap.to("#current-launchers-wrapper #r-gslv",{
             display:"flex"
         })
-        gsap.to("#cb3",{
+        gsap.to("#current-launchers-wrapper #cb3",{
             color:"#00bcd4"
         })
     })
     var lvm3=document.querySelector("#cb4")
     lvm3.addEventListener("click",function(){
-        gsap.to(".r-panel h3",{
+        gsap.to("#current-launchers-wrapper .r-panel h3",{
             color:"#2c5364"
         })
-        gsap.to(".launcher-box",{
+        gsap.to("#current-launchers-wrapper .launcher-box",{
             display:"none"
         })
-        gsap.to("#r-lvm3",{
+        gsap.to("#current-launchers-wrapper #r-lvm3",{
             display:"flex"
         })
-        gsap.to("#cb4",{
+        gsap.to("#current-launchers-wrapper #cb4",{
             color:"#00bcd4"
         })
     })
@@ -65,46 +65,46 @@ current_launchers()
 function upcoming_launchers(){
     var hrlv=document.querySelector("#fb1")
     hrlv.addEventListener("click",function(){
-        gsap.to(".r-panel2 h3",{
+        gsap.to(" #upcoming-launchers-wrapper  .r-panel2 h3",{
             color:"#2c5364"
         })
-        gsap.to(".launcher-box2",{
+        gsap.to("#upcoming-launchers-wrapper .launcher-box2",{
             display:"none"
         })
-        gsap.to("#r-hrlv",{
+        gsap.to(" #upcoming-launchers-wrapper  #r-hrlv",{
             display:"flex"
         })
-        gsap.to("#fb1",{
+        gsap.to("#upcoming-launchers-wrapper  #fb1",{
             color:"#00bcd4"
         })
     })
     var rlv=document.querySelector("#fb2")
     rlv.addEventListener("click",function(){
-        gsap.to(".r-panel2 h3",{
+        gsap.to("#upcoming-launchers-wrapper  .r-panel2 h3",{
             color:"#2c5364"
         })
-        gsap.to(".launcher-box2",{
+        gsap.to("#upcoming-launchers-wrapper  .launcher-box2",{
             display:"none"
         })
-        gsap.to("#r-rlv",{
+        gsap.to("#upcoming-launchers-wrapper  #r-rlv",{
             display:"flex"
         })
-        gsap.to("#fb2",{
+        gsap.to("#upcoming-launchers-wrapper  #fb2",{
             color:"#00bcd4"
         })
     })
     var td=document.querySelector("#fb3")
     td.addEventListener("click",function(){
-        gsap.to(".r-panel2 h3",{
+        gsap.to("#upcoming-launchers-wrapper  .r-panel2 h3",{
             color:"#2c5364"
         })
-        gsap.to(".launcher-box2",{
+        gsap.to("#upcoming-launchers-wrapper  .launcher-box2",{
             display:"none"
         })
-        gsap.to("#r-td",{
+        gsap.to("#upcoming-launchers-wrapper  #r-td",{
             display:"flex"
         })
-        gsap.to("#fb3",{
+        gsap.to("#upcoming-launchers-wrapper  #fb3",{
             color:"#00bcd4"
         })
     })
